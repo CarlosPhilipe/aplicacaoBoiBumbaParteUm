feb4fb53d83b461df3e1cfaefbd6fd40db9a3ff5
Carlos <carlosphbahia@gmail.com>
2016-04-18 14:36:38 -0400

    #Documentação | correção do script do banco, agora está toda a aplicação

--- documentacao/banco_boi_bumba.sql
+++ documentacao/banco_boi_bumba.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Apr 15, 2016 at 05:02 AM
+-- Generation Time: Apr 18, 2016 at 08:33 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.7
 
@@ -17,6 +17,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `apresentacao`
+--
+
+CREATE TABLE `apresentacao` (
+  `idapresentacao` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `data_hora_inicio` datetime DEFAULT NULL,
+  `data_hora_fim` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `elemento`
 --
 
@@ -26,19 +39,127 @@ CREATE TABLE `elemento` (
   `tempo` int(11) DEFAULT NULL,
   `descricao` text,
   `tipo_idtipo` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `elemento`
 --
 
 INSERT INTO `elemento` (`idelemento`, `nome`, `tempo`, `descricao`, `tipo_idtipo`) VALUES
-(1, 'novo', 12, 'aqui é novo mesmo', 1),
-(6, 'Elemento criado', 15, 'aqui é novo elemento criado', 2);
+(1, 'novo', 721, 'aqui é novo mesmo', 1),
+(19, 'Elemento criado', 740, 'Carlos', 1),
+(21, 'cântico', 570, 'Apresentador introduz a festa', 2),
+(22, 'Elemento criado', 984, 'as', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parte`
+--
+
+CREATE TABLE `parte` (
+  `idparte` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parte_compoem_roteiro`
+--
+
+CREATE TABLE `parte_compoem_roteiro` (
+  `parte_idparte` int(11) NOT NULL,
+  `roteiro_idroteiro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parte_contem_elemento`
+--
+
+CREATE TABLE `parte_contem_elemento` (
+  `elemento_idelemento` int(11) NOT NULL,
+  `parte_idparte` int(11) NOT NULL,
+  `ocorreu` binary(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roteiro`
+--
+
+CREATE TABLE `roteiro` (
+  `idroteiro` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `apresentacao_idapresentacao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `idtipo` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tipo`
+--
+
+INSERT INTO `tipo` (`idtipo`, `nome`) VALUES
+(1, 'Música'),
+(2, 'Trilha');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `nome` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `sexo` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '10',
+  `status` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '10',
+  `created_at` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `endereco` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `complemento` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `instituicao` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data_nasc` date DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `grupoacesso` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cpf` varchar(14) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tipo` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `nome`, `sexo`, `auth_key`, `password_hash`, `password_reset_token`, `role`, `status`, `created_at`, `updated_at`, `endereco`, `complemento`, `instituicao`, `data_nasc`, `email`, `grupoacesso`, `cpf`, `tipo`) VALUES
+(16, '201032', 'carlos philipe mendes bahia', '0', 'xg6I_LrtWiIHyQmS3_mSzIMFy_-BG9-5', '$2y$13$oJNvsY/slghrD6uTp/qO8uQJ0ohELFSCFc7sOErXt4sALE2qUo3dK', '', '10', '10', '1452973046', '1452973046', '', '', '', NULL, 'carlosphbahia@gmail.com', 'gestor', '', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `apresentacao`
+--
+ALTER TABLE `apresentacao`
+  ADD PRIMARY KEY (`idapresentacao`);
 
 --
 -- Indexes for table `elemento`
@@ -48,14 +169,81 @@ ALTER TABLE `elemento`
   ADD KEY `fk_elemento_tipo_idx` (`tipo_idtipo`);
 
 --
+-- Indexes for table `parte`
+--
+ALTER TABLE `parte`
+  ADD PRIMARY KEY (`idparte`);
+
+--
+-- Indexes for table `parte_compoem_roteiro`
+--
+ALTER TABLE `parte_compoem_roteiro`
+  ADD PRIMARY KEY (`parte_idparte`,`roteiro_idroteiro`),
+  ADD KEY `fk_parte_has_roteiro_roteiro1_idx` (`roteiro_idroteiro`),
+  ADD KEY `fk_parte_has_roteiro_parte1_idx` (`parte_idparte`);
+
+--
+-- Indexes for table `parte_contem_elemento`
+--
+ALTER TABLE `parte_contem_elemento`
+  ADD PRIMARY KEY (`elemento_idelemento`,`parte_idparte`),
+  ADD KEY `fk_elemento_has_parte_parte1_idx` (`parte_idparte`),
+  ADD KEY `fk_elemento_has_parte_elemento1_idx` (`elemento_idelemento`);
+
+--
+-- Indexes for table `roteiro`
+--
+ALTER TABLE `roteiro`
+  ADD PRIMARY KEY (`idroteiro`),
+  ADD KEY `fk_roteiro_apresentacao1_idx` (`apresentacao_idapresentacao`);
+
+--
+-- Indexes for table `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`idtipo`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `apresentacao`
+--
+ALTER TABLE `apresentacao`
+  MODIFY `idapresentacao` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `elemento`
 --
 ALTER TABLE `elemento`
-  MODIFY `idelemento` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `idelemento` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `parte`
+--
+ALTER TABLE `parte`
+  MODIFY `idparte` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `roteiro`
+--
+ALTER TABLE `roteiro`
+  MODIFY `idroteiro` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `idtipo` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
 -- Constraints for dumped tables
 --
@@ -65,3 +253,23 @@ ALTER TABLE `elemento`
 --
 ALTER TABLE `elemento`
   ADD CONSTRAINT `fk_elemento_tipo` FOREIGN KEY (`tipo_idtipo`) REFERENCES `tipo` (`idtipo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `parte_compoem_roteiro`
+--
+ALTER TABLE `parte_compoem_roteiro`
+  ADD CONSTRAINT `fk_parte_has_roteiro_parte1` FOREIGN KEY (`parte_idparte`) REFERENCES `parte` (`idparte`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_parte_has_roteiro_roteiro1` FOREIGN KEY (`roteiro_idroteiro`) REFERENCES `roteiro` (`idroteiro`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `parte_contem_elemento`
+--
+ALTER TABLE `parte_contem_elemento`
+  ADD CONSTRAINT `fk_elemento_has_parte_elemento1` FOREIGN KEY (`elemento_idelemento`) REFERENCES `elemento` (`idelemento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_elemento_has_parte_parte1` FOREIGN KEY (`parte_idparte`) REFERENCES `parte` (`idparte`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `roteiro`
+--
+ALTER TABLE `roteiro`
+  ADD CONSTRAINT `fk_roteiro_apresentacao1` FOREIGN KEY (`apresentacao_idapresentacao`) REFERENCES `apresentacao` (`idapresentacao`) ON DELETE NO ACTION ON UPDATE NO ACTION;
