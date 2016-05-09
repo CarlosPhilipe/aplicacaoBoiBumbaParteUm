bf3181d3ca7b106fceed52dae00dbf8c14241e70
Carlos <carlosphbahia@gmail.com>
2016-05-09 15:29:12 -0400

    alterado para produção

--- aplicacao/frontend/views/site/index.php
+++ aplicacao/frontend/views/site/index.php
@@ -8,45 +8,12 @@ $this->title = 'My Yii Application';
 
     <div class="jumbotron">
         <h1>IFestival</h1>
-
-        <p class="lead">Aqui entra a fase de efeito que causa boa impressão</p>
+        <p class="lead">Modernizando nossa cultura, atualizando nossas raizes</p>
 
     </div>
 
     <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>O que fazemos</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="#">Leia mais &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Porque fazemos</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="#">Leia mais &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Como fazemos</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="#">Leia mais &raquo;</a></p>
-            </div>
-        </div>
 
     </div>
 </div>
